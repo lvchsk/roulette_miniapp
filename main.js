b0c3b8a43510166da52690cf2060f23c771185e2
lvchsk <dienislev@mail.ru>
2025-01-11 15:31:08 +0300

    modalsUpdates

--- main.js
+++ main.js
@@ -10,10 +10,10 @@ window.addEventListener("load", async () => {
   const tg = window.Telegram.WebApp;
   tg.expand();
 
-  // const initData =
-  //   "query_id=AAFnEKlRAAAAAGcQqVFuPynM&user=%7B%22id%22%3A1370034279%2C%22first_name%22%3A%22PUG%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22mad_pug%22%2C%22language_code%22%3A%22ru%22%2C%22is_premium%22%3Atrue%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2FC4P153sbF5ZKQ0bXj61fro_kpL2AtsdVnAeOQ2veP_Y.svg%22%7D&auth_date=1734346396&signature=tkzPFjA-_u5sC8Jn29G8iy3729vIL8fyHHyALY4aVmsnqtNXcCalZzS4GmnglECiEcO1SdHCgFeJDUHyFiPiCg&hash";
+  const initData =
+    "query_id=AAFnEKlRAAAAAGcQqVFuPynM&user=%7B%22id%22%3A1370034279%2C%22first_name%22%3A%22PUG%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22mad_pug%22%2C%22language_code%22%3A%22ru%22%2C%22is_premium%22%3Atrue%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2FC4P153sbF5ZKQ0bXj61fro_kpL2AtsdVnAeOQ2veP_Y.svg%22%7D&auth_date=1734346396&signature=tkzPFjA-_u5sC8Jn29G8iy3729vIL8fyHHyALY4aVmsnqtNXcCalZzS4GmnglECiEcO1SdHCgFeJDUHyFiPiCg&hash";
 
-  const initData = tg.initData;
+  // const initData = tg.initData;
 
   const loading = document.getElementById("loading");
   loading.style.display = "flex";
@@ -47,13 +47,13 @@ window.addEventListener("load", async () => {
     const diff = nextGiftTime - now;
 
     if (diff <= 0) {
-      freeSpinButton.addEventListener("touchstart", giftHandler);
+      freeSpinButton.addEventListener("pointerdown", giftHandler);
       timerElement.innerText = "";
       freeSpinButton.disabled = false;
       freeSpinButton.innerText = "Получить";
       clearInterval(timerInterval);
     } else {
-      freeSpinButton.removeEventListener("touchstart", giftHandler);
+      freeSpinButton.removeEventListener("pointerdown", giftHandler);
       freeSpinButton.disabled = true;
       freeSpinButton.classList.add("disabled_btn");
       timerElement.classList.add("disabled_timer");
@@ -99,23 +99,27 @@ window.addEventListener("load", async () => {
     setTimeout(() => {
       btnMinus.disabled = false;
       console.log("Кнопка снова активна");
-    }, 7500);
+    }, 9600);
 
     const result = await updateSpins(initData);
+    console.log(result);
+    
 
     spins = result.spins;
     updateSpinsDisplay(spins);
 
     const prize = result.prize.value || "";
     const degree = result.prize.degree;
+    const link = result.prize.link;
     console.log(`Приз: ${prize}, Угол: ${degree}`);
 
     rotateWheel(degree);
-    prizeModals(prize);
+    prizeModals(prize, link);
   }
 
-  btnMinus.addEventListener("click", handleButtonClick);
-  btnMinus.addEventListener("touchstart", handleButtonClick);
+  // btnMinus.addEventListener("click", handleButtonClick);
+  // btnMinus.addEventListener("touchstart", handleButtonClick);
+  btnMinus.addEventListener('pointerdown', handleButtonClick);
 
   const currentPath = window.location.pathname.replace(
     /^\/Pages\/(.+)/,
