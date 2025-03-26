9dd2414ec13b008acc99c904e29e83ae7f14af48
lvchsk <dienislev@mail.ru>
2025-03-26 23:30:40 +0300

    minorChanges3

--- Scripts/invite.js
+++ Scripts/invite.js
@@ -1,27 +1,26 @@
 export function inviteFriend(tg, referralLink) {
-    if (tg.openContactPicker) {
-        tg.openContactPicker({
-          message: `Переходи по ссылке в лучшую Telegram рулетку, вращай и получай купоны от 500₽ до 30000₽ на WB: ${referralLink}`,
-        })
-          .then((result) => {
-            if (result && result.contacts) {
-              alert(`Сообщение отправлено ${result.contacts.length} контактам!`);
-            } else {
-              alert("Отмена выбора контактов.");
-            }
-          })
-          .catch((error) => {
-            console.error("Ошибка при открытии контакт-пикера:", error);
-            // alert("Не удалось открыть список контактов.");
-            alert(error);
-          });
-      } else {
-        const message = encodeURIComponent(
-          `Переходи по ссылке в лучшую Telegram рулетку, вращай и получай купоны от 500₽ до 30000₽ на WB:`
-        );
-        window.open(
-          `https://t.me/share/url?url=${referralLink}&text=${message}`,
-          "_blank"
-        );
-      }
+  if (tg.openContactPicker) {
+    tg.openContactPicker({
+      message: `Переходи по ссылке, крути колесо фортуны и получай ценные призы и звёзды в Telegram! 🎁\nФортуна на твоей стороне — не упусти момент! 🌟\n${referralLink}`,
+    })
+      .then((result) => {
+        if (result && result.contacts) {
+          alert(`Сообщение отправлено ${result.contacts.length} контактам!`);
+        } else {
+          alert("Отмена выбора контактов.");
+        }
+      })
+      .catch((error) => {
+        console.error("Ошибка при открытии контакт-пикера:", error);
+        alert(error);
+      });
+  } else {
+    const message = encodeURIComponent(
+      `Переходи по ссылке, крути колесо фортуны и получай ценные призы и звёзды в Telegram! 🎁\nФортуна на твоей стороне — не упусти момент! 🌟\n`
+    );
+    window.open(
+      `https://t.me/share/url?url=${referralLink}&text=${message}`,
+      "_blank"
+    );
+  }
 }
